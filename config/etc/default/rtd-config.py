d2209bf06ec002d562624b507a2645e05f0f3955
Jeff Sternberg <jeffsternberg@gmail.com>
2016-08-27 12:00:38 -0400

    tweaked config for local builds and private projects

--- config/etc/default/rtd-config.py
+++ config/etc/default/rtd-config.py
@@ -24,3 +24,11 @@ BROKER_URL = 'redis://redis:6379/0'
 CELERY_RESULT_BACKEND = 'redis://redis:6379/0'
 DEBUG = True
 CELERY_ALWAYS_EAGER = False
+
+ALLOW_PRIVATE_REPOS = True
+DEFAULT_PRIVACY_LEVEL = 'private'
+DEFAULT_VERSION_PRIVACY_LEVEL = 'private'
+
+SLUMBER_API_HOST = 'http://localhost:8000'
+PRODUCTION_DOMAIN = 'http://localhost:8000'
+WEBSOCKET_HOST = 'http://localhost:8000'
